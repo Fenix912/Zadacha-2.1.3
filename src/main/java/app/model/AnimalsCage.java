1a820627d37204c4d2ddf59d5dc380c080d2e218
Fenix912 <maggie991@bk.ru>
2022-01-27 20:40:14 +0300

    Zadacha-2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,22 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("animalsCageBean") //Данная аннатация используется для (Сканирования) регистрации бинов.
 public class AnimalsCage {
 
-    @Autowired
+    @Autowired            // Данная аннатация используется для внедрения зависимости.
+    @Qualifier("dogBean") // Данная аннатация используется для уточнения(какой бин использовать).
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
